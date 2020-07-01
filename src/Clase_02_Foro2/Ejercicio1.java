f4fe66eeed0190c84a5a9d614610085c312740cc
juan.fernandez1 <juan.fernandez@sitrack.com>
2020-07-01 01:30:06 -0300

    Clase02Foro2

---
+++ src/Clase_02_Foro2/Ejercicio1.java
@@ -0,0 +1,20 @@
+package Clase_02_Foro2;
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String[] oracion = new String[4];
+        for (int i = 0; i < oracion.length; i++) {
+            System.out.println("Ingrese una oracion");
+            String newOracion = input.nextLine();
+            if(!newOracion.equals("")){
+                oracion[i] = newOracion;
+            }
+        }
+        for (int i = 0; i< oracion.length; i++){
+            System.out.println("----------");
+            System.out.println(oracion[i]);
+        }
+    }
+}
---
+++ src/Clase_02_Foro2/Ejercicio2.java
@@ -0,0 +1,43 @@
+package Clase_02_Foro2;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String[] users = {"Daiana", "Facundo", "Osvaldo", "Melina"};
+        String[] pass = {"1234", "4321", "1122", "3344"};
+        String message = "";
+        boolean bandera = false;
+        for (int i = 0; i < 10; i++){
+            System.out.println("Welcome to Lavalle");
+            System.out.println("User:");
+            String user = input.nextLine();
+            System.out.println("password:");
+            String password = input.nextLine();
+            if ((!user.equals("")) && (!password.equals(""))){
+                for (int j=0; j < users.length; j++){
+                    if(user.equals(users[j])){
+                        if(password.equals(pass[j])){
+                            System.out.println(" --------------");
+                            System.out.println(" | Bienvenido " + users[j]);
+                            System.out.println(" --------------");
+                            bandera = true;
+                            break;
+                        }else{
+                            message = "Contraseña Incorrecto";
+                            break;
+                        }
+                    }else{
+                        message = "Usuario Incorrecto";
+                    }
+                }
+                if(bandera != true){
+                    System.out.println("********* " + message + " +++++++");
+                }
+            }else{
+                message = "COMPLETE LOS CAMPOS";
+            }
+        }
+    }
+}
