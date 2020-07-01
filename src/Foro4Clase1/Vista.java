1698b93a09a99669d1c84754ab434d133b517139
juan.fernandez1 <juan.fernandez@sitrack.com>
2020-06-30 21:35:31 -0300

    funcionalidad foro 4

---
+++ src/Foro4Clase1/Vista.java
@@ -0,0 +1,15 @@
+package Foro4Clase1;
+
+public class Vista {
+    public static String leerAtributo(String atributo){
+        if(!atributo.equals(null)){
+            return atributo;
+        }else{
+            return "error";
+        }
+    }
+    public static void escribirAtributo(String atributo, String valor){
+        System.out.println(String.format("Atributo%s ",atributo));
+        System.out.println(String.format("Valor%s ",valor));
+    }
+}
